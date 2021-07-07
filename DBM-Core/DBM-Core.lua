f1476db69fcb7b4fb9180e9988d8bc5d93c33c56
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2021-07-07 23:26:24 +0400

    fix

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -69,7 +69,7 @@ local function showRealDate(curseDate)
 end
 
 DBM = {
-	Revision = parseCurseDate("20210628070000"),
+	Revision = parseCurseDate("20210708130000"),
 	DisplayVersion = "5.54", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2021, 05, 02) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
